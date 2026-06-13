08b98817bc739095edf4a6aa701f0dab73577a9e
youssef1347 <husseinyoussef589@gmail.com>
2026-06-13 14:26:28 -0700

    ...

--- back-end/index.js
+++ back-end/index.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 initSocket(server);
 
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
